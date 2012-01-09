083f4aef33385620a6282021387a8d5e146c3e95
MACHIZAUD Andréa <andrea.machizaud@gmail.com>
2012-01-09 01:56:28 +0100

    Added game end,(win and loss) event

--- Trash2012/Model/Company.cs
+++ Trash2012/Model/Company.cs
@@ -6,64 +6,14 @@ namespace Trash2012.Model
 {
     public class Company
     {
-        public Resource Gold { get; set; }
+        public int Gold { get; set; }
         public List<Truck> Trucks { get; set; }
 
-        public Company()
+        public Company(int initialAmount)
         {
-            Gold = new Resource("Or", 0, 10000000, 1000000);
+            Gold = initialAmount;
             Trucks = new List<Truck>(10);
         }
-
-    }
-
-    public class Resource
-    {
-        public readonly string Name;
-        public readonly int Minimum;
-        public readonly int Maximum;
-        public int Current { get; set; }
-
-        public Resource(string name, int min, int max, int value)
-        {
-            Name = name;
-            Minimum = min;
-            Maximum = max;
-            Current = value;
-        }
-
-        public void Add(int quantity)
-        {
-            if (quantity + Current > Maximum) Current = Maximum;
-            else if(quantity + Current < Minimum) Current = Minimum;
-            else  Current += quantity;
-        }
-
-        #region Syntaxic Sugar Helpers
-
-        public static implicit operator Resource(int value)
-        {
-            return new Resource("anything", Int32.MinValue, Int32.MaxValue, value);
-        }
-
-        public static Resource operator+(Resource a, Resource b)
-        {
-            a.Add(b.Current);
-            return a;
-        }
-
-        public static Resource operator -(Resource a, Resource b)
-        {
-            a.Add(-b.Current);
-            return a;
-        }
-
-        public static Resource operator /(Resource a, Resource b)
-        {
-            return a.Current/b.Current;
-        }
-
-        #endregion
 
     }
 }
